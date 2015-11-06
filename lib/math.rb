59cf4de8ccbfad9f8beec1fa12bf561943f78d2a
KevinDTran <kevin@zipmark.com>
2015-11-05 19:26:13 -0600

    Done.

--- lib/math.rb
+++ lib/math.rb
@@ -1,21 +1,44 @@
+require 'Math'
+
 def addition(num1, num2)
+  a = num1 + num2
+  puts a
+  return a
 end
 
 def subtraction(num1, num2)
+  a = num1 - num2
+  puts a
+  return a
 end
 
 def division(num1, num2)
+  a = num1/num2
+  puts a
+  return a
 end
 
 def multiplication(num1, num2)
+  a = num1 * num2
+  puts a
+  return a
 end
 
 def modulo(num1, num2)
+  a = num1 % num2
+  puts a
+  return a
 end
 
 def square_root(num)
+  a = Math.sqrt(num)
+  puts a
+  return a
 end
 
 def order_of_operation(num1, num2, num3, num4)
   #Hint:  __ + (( __ * __ ) / __ )
+  a =num1 + ((num2*num3)/num4)
+  puts a
+  return a
 end
